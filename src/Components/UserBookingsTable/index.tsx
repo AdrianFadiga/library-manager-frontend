8c0e503ed73c69304154301a9efd31a4e758bd7e
AdrianFadiga <adrianmfadiga@gmail.com>
2022-10-11 01:11:56 -0300

    feat: pagina de filtrar reservas de admin

---
+++ src/Components/UserBookingsTable/index.tsx
@@ -0,0 +1,35 @@
+import { IBooking } from '../../Interfaces/IBooking';
+import Table from 'react-bootstrap/Table';
+
+interface Props {
+    bookings: IBooking[]
+}
+
+const UserBookingsTable: React.FC<Props> = ({bookings}) => {
+  return (
+    <Table striped bordered hover>
+      <thead>
+        <tr>
+          <th>#</th>
+          <th>Title</th>
+          <th>Booking Date</th>
+          <th>Return Date</th>
+          <th>Status</th>
+        </tr>
+      </thead>
+      <tbody>
+        {bookings.map(({book: {title}, bookingDate, returnDate, status, id}, i) => (
+          <tr key={id}>
+            <td>{i + 1}</td>
+            <td>{title}</td>
+            <td>{bookingDate}</td>
+            <td>{returnDate}</td>
+            <td>{status}</td>
+          </tr>
+        ))}
+      </tbody>
+    </Table>
+  );
+};
+
+export default UserBookingsTable;
